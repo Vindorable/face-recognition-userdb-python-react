e066e19b944ca531554e6581efee7105d023c5de
Vindorable <j.raj93.designer@gmail.com>
2024-01-23 15:37:52 +0800

    Login page form with validation.

--- facial-recognition-userdatabase/client/src/pages/login.jsx
+++ facial-recognition-userdatabase/client/src/pages/login.jsx
@@ -1,12 +1,51 @@
 import React from "react";
-import { Divider, Stack, Typography } from "@mui/material";
+import { Button, Divider, Stack, TextField, Typography } from "@mui/material";
 
 import BodyWrapper from "../components/body-wrapper";
+
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+
+// ---------------------------------------------------------
+
+const getCharacterValidationError = (str) => {
+  return `Your password must have at least 1 ${str} character.`;
+};
+
+const validationSchema = yup.object({
+  email: yup
+    .string("Enter your email.")
+    .email("Enter a valid email.")
+    .required("Email is required."),
+  password: yup
+    .string("Enter your password.")
+    .min(8, "Password must have at least 8 characters.")
+    .matches(/[0-9]/, getCharacterValidationError("digit"))
+    .matches(/[a-z]/, getCharacterValidationError("lowercase"))
+    .matches(/[A-Z]/, getCharacterValidationError("uppercase"))
+    .required("Password is required."),
+  confirmPassword: yup
+    .string("Please re-type your password.")
+    .oneOf([yup.ref("password")], "Passwords does not match."),
+});
 
 
 // ---------------------------------------------------------
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <>
       <BodyWrapper>
@@ -16,6 +55,50 @@ const Login = () => {
           <Typography variant="body2">Don't have an account, yet? <b>Create Account</b></Typography>
         </Stack>
         <Divider />
+
+        <Divider sx={{ visibility: "hidden", borderBottomWidth: 10 }} />
+
+        <form onSubmit={formik.handleSubmit}>
+          <Stack spacing={2}>
+            <TextField
+              fullWidth
+              id="email"
+              name="email"
+              label="* Email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+
+            <TextField
+              fullWidth
+              id="password"
+              name="password"
+              label="* Password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+
+            <TextField
+              fullWidth
+              id="confirmPassword"
+              name="confirmPassword"
+              label="* Confirm Password"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+              helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+            />
+
+            <Button type="submit">Submit</Button>
+          </Stack>
+        </form>
       </BodyWrapper>
     </>
   );
