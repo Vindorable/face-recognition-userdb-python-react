9953e5c18afdd2aafd3df3b11da11b4845763157
Vindorable <j.raj93.designer@gmail.com>
2024-01-24 08:14:44 +0800

    Setup python flask backend.

---
+++ facial-recognition-userdatabase/backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+
+# ---------------------------------------------------------
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+app = create_app()
+
+
+# ---------------------------------------------------------
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+# ---------------------------------------------------------
+
+if __name__ == "__main__":
+    app.run(debug=True)
